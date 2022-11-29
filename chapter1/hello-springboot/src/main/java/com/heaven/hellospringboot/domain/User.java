44c75738a218f41adbc5732b7f723ffc46aca115
Quang <tomminhquan1411@gmail.com>
2022-11-29 18:16:53 +0700

    SpringBoot 3 Chapter 1 JPA MVC

---
+++ chapter1/hello-springboot/src/main/java/com/heaven/hellospringboot/domain/User.java
@@ -0,0 +1,18 @@
+package com.heaven.hellospringboot.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+}
---
+++ chapter1/hello-springboot/src/main/java/com/heaven/hellospringboot/web/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.heaven.hellospringboot.web.controllers;
+
+import com.heaven.hellospringboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserRepository userRepo;
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("users",userRepo.findAll());
+        return "index";
+    }
+}
